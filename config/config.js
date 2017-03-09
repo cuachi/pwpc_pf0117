861e9b5c99f00f81cf20d66ebd29d56cf0c46670
cuachi <jagarfiasi@outlook.com>
2017-03-09 08:17:09 -0600

    00 Iniciando Proyecto

---
+++ config/config.js
@@ -0,0 +1,11 @@
+exports.module = {
+    "IP": process.env.IP || '127.0.0.1',
+    "PORT": process.env.PORT || 3000,
+    "color_theme": {
+        "info": "rainbow",
+        "data": "green",
+        "error": "red",
+        "warning": "yellow"
+    },
+    "STATIC_PATH": "./static"
+};
