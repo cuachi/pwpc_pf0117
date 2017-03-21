0dd9c42a02dd4aeb91442be7c7da029fe2fd14bc
cuachi <jagarfiasi@outlook.com>
2017-03-21 11:33:10 -0600

    Reloj

--- internals/handlers.js
+++ internals/handlers.js
@@ -4,6 +4,26 @@ var author = {
     "university" : "TecNM - ITGAM" 
 };
 
+var getservertime = function(req, res){
+    res.writeHead(200,{
+        "Content-Type" : "text/html"
+    });
+    var date = new Date();
+    var horas = date.getHours();
+    var minutos = date.getMinutes();
+    var segundos = date.getSeconds();
+    if (horas <= 24 && horas <= 12 ) {
+        res.write('<img src="http://127.0.0.1:3000/img/amanecer.png" width=100 heigth=100> </img>');
+        res.end(`<h1>Buenos dias a hora del server es: ${horas}:${minutos}:${segundos}</h1>`);
+    } else if(horas > 12 && horas <= 18){
+        res.write('<img src="http://127.0.0.1:3000/img/atardecer.jpg" width=100 heigth=100> </img>');
+        res.end(`<h1>Buenas tardes la hora del server es: ${horas}:${minutos}:${segundos}</h1>`);
+    }else{
+        res.write('<img src="http://127.0.0.1:3000/img/amanecer.png" width=100 heigth=100> </img>');
+        res.end(`<h1>Buenas noches la hora del server es: ${horas}:${minutos}:${segundos}</h1>`);
+    }
+};
+
 var getauthorinfo = function(req, res) {
 
     res.writeHead(200,{
@@ -22,4 +42,5 @@ var handlers = {};
 
 handlers["/getauthorinfo"] = getauthorinfo;
 handlers["/getServerName"] = getServerName;
+handlers["/getservertime"] = getservertime;
 module.exports = handlers;
